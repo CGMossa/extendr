1e7905ea819a9a3f1ae6cb663c108e28f6c596f2
cgmossa <cgmossa@gmail.com>
2024-01-21 15:42:57 +0100

    doc: fixes

--- extendr-api/src/scalar/rbool.rs
+++ extendr-api/src/scalar/rbool.rs
@@ -124,7 +124,7 @@ impl TryFrom<&Robj> for Rbool {
     type Error = Error;
 
     /// Convert an `LGLSXP` object into a `Rbool` (tri-state boolean).
-    /// Use `value.is_na()` to detect NA values.
+    /// Use `value.is_na()` to detect `NA` values.
     fn try_from(robj: &Robj) -> Result<Self> {
         if let Some(v) = robj.as_logical_slice() {
             match v.len() {
