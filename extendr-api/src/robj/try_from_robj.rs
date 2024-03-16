38737a2beefadec803bb3e2c90e82f6e53c0c2fb
Mossa <cgmossa@gmail.com>
2024-03-17 00:47:58 +0100

    fix? You can now generate this from a reference to an `Robj`. Tests are missing.

--- extendr-api/src/robj/try_from_robj.rs
+++ extendr-api/src/robj/try_from_robj.rs
@@ -429,6 +429,7 @@ impl_try_from_robj_ref!(
     bool
     Rint Rfloat Rbool Rcplx
     f32 f64
+    HashMap::<String, Robj> HashMap::<&str, Robj>
     Vec::<String>
     Vec::<Rint> Vec::<Rfloat> Vec::<Rbool> Vec::<Rcplx> Vec::<u8> Vec::<i32> Vec::<f64>
     &[Rint] &[Rfloat] &[Rbool] &[Rcplx] &[u8] &[i32] &[f64]
